6b885e7eb65b9290abb233689b4f86f522ef0a08
jacelynfish <hyjin.jacelyn@gmail.com>
2017-12-12 02:14:25 +0800

    modify proxy table

--- config/index.js
+++ config/index.js
@@ -6,15 +6,13 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-        '/get_expr': 'http://104.236.63.92:3030',
-        '/eval_expr': 'http://localhost:3000'
-    },
-
+    proxyTable: [{
+      context: ['/get_expr', '/eval_expr'],
+      target: 'http://104.236.63.92:3030'
+    }],
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
